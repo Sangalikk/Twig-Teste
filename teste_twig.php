1f273ef3e8a89c389b333b6385fe55879d3b4f84
Lucas Sangali <165666224+Sangalikk@users.noreply.github.com>
2026-03-16 16:58:12 -0300

    Add Poema

--- teste_twig.php
+++ teste_twig.php
@@ -10,9 +10,26 @@ $disciplinas = [
     'Desenvolvimento de Sistemas',
 ];
 
+$poema = '
+    Um dia, há seis anos, 
+    um arco-íris delicado e esmaecido desenhou-se no meu peito, 
+    e muito embora não se tratasse nem de amor nem de bem-querer, 
+    à medida que passavam os meses e os anos, esse arco-íris foi 
+    carregando as suas cores com grande beleza, e, até agora, 
+    jamais o perdi de vista. O arco-íris que surge no céu limpo 
+    depois de uma chuva tem existência efêmera e logo se apaga, 
+    mas o que nasce no coração da gente parece que não. 
+    Por favor, peço-lhe que o interpele. O que acha realmente de mim?
+    Será que me tem em conta de um arco-íris depois da chuva? Que se apagou já há muito tempo?
+
+    Se assim for, eu também tenho que apagar o meu arco-íris. 
+    Mas sinto que ele não quer sumir-se antes de eu mesma terminar com minha vida.
+    ';
+
 echo $twig->render('teste_twig.html', [
     'nome'=> $nome,
     'legal'=> true,
     'disciplinas'=> $disciplinas,
+    'poema'=> $poema,
 ]);
 
