107e40f337a7689764a656dd2613354fdeb49af0
Lucas Sangali <165666224+Sangalikk@users.noreply.github.com>
2026-03-16 16:16:12 -0300

    inicio aula 031626

--- teste_twig.php
+++ teste_twig.php
@@ -3,9 +3,16 @@
 require_once('carregar_twig.php');
 
 $nome = 'Fulaninho';
+$disciplinas = [
+    'Programação',
+    'Banco de Dados',
+    'Interface Web',
+    'Desenvolvimento de Sistemas',
+];
 
 echo $twig->render('teste_twig.html', [
     'nome'=> $nome,
     'legal'=> true,
+    'disciplinas'=> $disciplinas,
 ]);
 
