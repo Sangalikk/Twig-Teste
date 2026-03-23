7801adbfd95d56b36290cf6ca0fdd3ce3d1b5b13
Lucas Sangali <165666224+Sangalikk@users.noreply.github.com>
2026-03-23 17:08:31 -0300

    tabela de jogos

--- jogos.php
+++ jogos.php
@@ -1,5 +1,12 @@
 <?php 
 //jogos php 
 require('carregar_twig.php'); 
+require('carregar_pdo.php');
 
-echo $twig->render('jogos.html');
+$jogos = $pdo->query('SELECT * FROM jogos');
+$todosJogos = $jogos->fetchAll(PDO::FETCH_ASSOC);
+
+
+echo $twig->render('jogos.html', [
+    'jogos' => $todosJogos
+]);
--- jogos_inserir.php
+++ jogos_inserir.php
@@ -9,13 +9,27 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(!$nome || !$estilo) {
         $erro = "Preencha os campos corretamente.";
     }else{
-        
+        // Tudo certo - gravar os dados
+        $ext = pathinfo($_FILES['capa']['name'], PATHINFO_EXTENSION);
+        $capa = uniqid() . "." . $ext;
+
+        move_uploaded_file($_FILES['capa']['tmp_name'], "img/{$capa}");
+
+        require("carregar_pdo.php");
+          $dados = $pdo->prepare("INSERT INTO jogos (nome, estilo, capa) VALUES (?, ?, ?)");
+          $dados->bindParam(1, $nome);
+          $dados->bindParam(2, $estilo);
+          $dados->bindParam(3, $capa);
+
+          $dados->execute();
+
+          header('location: jogos.php');
+          die; 
     }
-     
 }
 
 require('carregar_twig.php');
 
 echo $twig->render('jogos_inserir.html', [
     'erro' => $erro,
-]);
+]); 
