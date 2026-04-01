7c432d54cf978c72165a69b905ef330799dcc184
Lucas Sangali <165666224+Sangalikk@users.noreply.github.com>
2026-04-01 11:18:00 -0300

    editar

--- jogos_editar.php
+++ jogos_editar.php
@@ -1,1 +1,20 @@
 <?php
+// jogos_editar.php
+require("carregar_pdo.php");
+require ("carregar_twig.php");
+
+$id = (int) $_GET["id"] ?? false;
+
+if (!$id) {
+    header("Location: jogos.php");
+    die;
+}else{
+    $dados = $pdo->prepare("SELECT * FROM jogos where id = :id");
+    $dados->execute(["id" => $id]);
+    $jogo = $dados->fetch(PDO::FETCH_ASSOC);
+
+}
+
+echo $twig->render("jogos_editar.html", [
+    'jogo' => $jogo, 
+]);
